c1144de9de98f8953cf39b83638042797578fdf3
SilvanoLopez <rafaelytotolandia9@gmail.com>
2020-07-01 14:22:29 -0400

    3er

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const app = express();
 
 //morgan
 const morgan = require('morgan');
+//Mongo
+const mongoose = require('mongoose');
 //cors
 const cors = require('cors');
 // Para acceder al directorio actual
 const path = require('path');
-
+const uri = 'mongodb://localhost:27017/restaurante_simple';
+const options = {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}; 
+ 
+// Or using promises
+mongoose.connect(uri, options).then(
+  /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
+  () => { console.log('Conectado a DB') },
+  /** handle initial connection error */
+  err => { console.log(err) }
+);
 app.use(morgan('tiny'));
 app.use(cors());
 //json
@@ -20,6 +31,7 @@ app.use(express.urlencoded({ extended: true }))
 const history = require('connect-history-api-fallback');
 app.use(history());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', require('./router/datainventario'));
 
 //app.get('/', function (req, res) {
   //res.send('Hello World!');
